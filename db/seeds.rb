d936a877efe4c54a5b98a893a05c41020bbcd8c1
Luke Grayland <lgrayland@gmail.com>
2015-01-17 12:11:22 +0000

    added some group seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,8 @@ User.delete.all
 User.create!(email: "luke@luke.com", password: "password", first_name: "Luke", last_name: "Grayland")
 User.create!(email: "andres@andres.com", password: "password", first_name: "Andres", last_name: "Idontknow")
 User.create!(email: "neil@neil.com", password: "password", first_name: "Neil", last_name: "Crosbourne")
+
+Group.delete.all
+
+Group.create!(name: "House share", description: "A group for a house share of 3 people")
+Group.create!(name: "House share 2", description: "A group for a flat share of 2 people")
