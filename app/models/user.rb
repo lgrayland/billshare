c51d167799c91773e55aac5857786f13cec4ac9f
Luke Grayland <lgrayland@gmail.com>
2015-01-17 11:20:29 +0000

    added associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
+
+  has_many :groupings
+  has_many :groups, through :groupings
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
 end
