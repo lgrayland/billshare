2e540b6d9ddab8766729e1d5f77f2d396a0af2bb
Andres Vara <andres.vara@gmail.com>
2015-01-18 22:36:02 +0000

    added validates to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
 
 
   has_many :groupings
