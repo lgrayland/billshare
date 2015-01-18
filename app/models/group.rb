a01abff3152016b27a39e60d3cc402cba27af4b3
Andres Vara <andres.vara@gmail.com>
2015-01-18 23:46:24 +0000

    add controller show test

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,6 +5,10 @@ class Group < ActiveRecord::Base
   has_many :bills
   has_many :bill_types
 
+  # before_save do
+  #   raise "no users in group!" if users.empty?
+  # end
+
   scope :group_bill_types, -> (group_id) { find(group_id).bill_types }
 
   def calculate_total
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -8,18 +8,36 @@ describe Group do
     # party_girls = Bill.new(name: "Girls", amount: 12, deadline: "2015.2.15")
     # party_music = Bill.new(name: "Music", amount: 8, deadline: "2015.2.15")
 
-    party_drinks = FactoryGirl.create(:bill)
-    party_fruits = FactoryGirl.create(:bill)
-    party_girls = FactoryGirl.create(:bill)
-    party_music = FactoryGirl.create(:bill)
+    party_drinks = create(:bill)
+    party_fruits = create(:bill)
+    party_girls = create(:bill)
+    party_music = create(:bill, amount: 300)
+
+    user1 = create(:user)
     
 
-    party = FactoryGirl.create(:group)
+    party = create(:group)
+    party.users << user1
 
     party.bills.push(party_drinks, party_fruits, party_girls, party_music)
 
     party.calculate_total
-    expect(party.total).to eq(800)
+    expect(party.total).to eq(900)
 
   end
+
+  # it "reises exception if group no users in it " do 
+  #   expect( -> { create(:group) } ).to raise_exception
+  # end
+
 end
+
+
+
+
+
+
+
+
+
+
