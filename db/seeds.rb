5f98fc57d0b0d81ae55917c23dd27ae3e135a46d
Andres Vara <andres.vara@gmail.com>
2015-01-18 15:43:45 +0000

    refactor bill_type and grope relations (has_many, belong_to)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 User.delete_all
 Group.delete_all
 Grouping.delete_all
@@ -14,13 +13,12 @@ Bill.delete_all
 BillType.delete_all
 Proportion.delete_all
 
-
 luke = User.create!(email: "luke@luke.com", password: "password", first_name: "Luke", last_name: "Grayland")
 andres = User.create!(email: "andres@andres.com", password: "password", first_name: "Andres", last_name: "Idontknow")
 neil = User.create!(email: "neil@neil.com", password: "password", first_name: "Neil", last_name: "Crosbourne")
 
 gr1 = Group.create!(name: "House share", description: "A group for a house share of 3 people")
-gr2 = Group.create!(name: "House share 2", description: "A group for a flat share of 2 people")
+# gr2 = Group.create!(name: "House share 2", description: "A group for a flat share of 2 people")
 
 gr1.users.push(luke, andres, neil)
 
@@ -41,5 +39,33 @@ tv_pr4 = Proportion.create!(percentage: 25)
 
 tv_type.bills.push(tv)
 tv_type.proportions.push(tv_pr1, tv_pr2, tv_pr3, tv_pr4)
+#########################
+
+u1 = User.create!(email: "u1@u1.com", password: "password", first_name: "u1", last_name: "u1l")
+u2 = User.create!(email: "u2@u2.com", password: "password", first_name: "u2", last_name: "u2l")
+u3 = User.create!(email: "u3@u3.com", password: "password", first_name: "u3", last_name: "u3l")
+
+party = Group.create!(name: "Party", description: "All night party")
+party.users.push(u1, u2, u3)
+
+party_drinks = Bill.create!(name: "Drinks", amount: 430, deadline: "2015.2.15")
+party_fruits = Bill.create!(name: "Fruits", amount: 210, deadline: "2015.2.15")
+party_girls = Bill.create!(name: "Girls", amount: 1230, deadline: "2015.2.15")
+party_music = Bill.create!(name: "Music", amount: 880, deadline: "2015.2.15")
+
+party_type = BillType.create!(name: "party")
+party.bills.push(party_drinks, party_fruits, party_girls, party_music)
+party_type.bills.push(party_drinks, party_fruits, party_girls, party_music)
+
+
+
+
+
+
+
+
+
+
+
 
 
