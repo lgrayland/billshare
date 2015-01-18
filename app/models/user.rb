af85371079b85446c7b9c5ce3115d4707b362387
Andres Vara <andres.vara@gmail.com>
2015-01-18 00:00:33 +0000

    add scope user_bills

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,15 @@ class User < ActiveRecord::Base
   has_many :groupings
   has_many :groups, through: :groupings
 
-  # scope :user_bills, -> (user_id) {  }
+  scope :user_bills, -> (user_id, group_id)   { find_by_id(user_id).groups.find_by_id(group_id).bills }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
-  def self.user_bills(user_id)
-    # find(self.id).groups
-    where(id: user_id)
-  end
+  # def self.user_bills(user_id)
+  #   # find(self.id).groups
+  #   where(id: user_id)
+  # end
 
 end
