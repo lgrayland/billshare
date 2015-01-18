3e65b90418f359a9156a5c3fb5815652085884d9
Luke Grayland <lgrayland@gmail.com>
2015-01-18 17:35:58 +0000

    added update method to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to(@user)
+  end
+
   private
-  
+
   def set_resources
     # @user = User.find(params[:id])
     # @groups = Group.where
