2063199c6e8160e63d739accdea4dc290a2a2e00
crosbourne <neilcrosbourne@hotmail.com>
2015-01-19 14:00:34 +0000

    added omniauth_callbacks_controller.rb

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,13 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    if user.persisted?
+      flash.notice = "Signed in Through Facebook"
+      sign_in_and_redirect user
+    else
+      session["devise.user_attributes"] = user.attributes
+      flash.notice = "Problem creating account"
+      redirect_to new_user_registration_url
+    end
+  end
+endomniauth_callbacks_controller.rb
