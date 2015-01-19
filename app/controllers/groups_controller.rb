4508b36f2a71619c684a2a28796d559b9dbba1af
Luke Grayland <lgrayland@gmail.com>
2015-01-19 15:19:59 +0000

    added create method to group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,12 @@
 class GroupsController < ApplicationController
   before_action :set_resources, only: [:show, :edit]
-  
+
+  def new
+    @group = Group.new
+  end
+
   def show
     @group = Group.find(params[:id])
-
-
   end
 
   private
