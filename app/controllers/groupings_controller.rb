0dfc66f3cc7da79400e0f6b649aa86667b71897e
Luke Grayland <lgrayland@gmail.com>
2015-01-19 17:26:57 +0000

    can add users to group through groupings

--- app/controllers/groupings_controller.rb
+++ app/controllers/groupings_controller.rb
@@ -1,7 +1,20 @@
 class GroupingsController < ApplicationController
 
   def new
-    
+    @group = Group.find(params[:group_id])
+    @grouping = Grouping.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @grouping = @group.groupings.new(grouping_params)
+    @grouping.save
+    redirect_to(@group)
+  end
+
+private
+  def grouping_params
+    params.require(:grouping).permit(:user_id)
   end
 
 end
