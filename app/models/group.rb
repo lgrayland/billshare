06917ccb35658bf5179bc206a33f41dc7d97175e
Andres Vara <andres.vara@gmail.com>
2015-01-21 10:11:58 +0000

    added validate user uniqueness in group

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,12 +5,13 @@ class Group < ActiveRecord::Base
   has_many :bills
   has_many :bill_types
 
-  before_save :user_in_group
 
-  def user_in_group
-    arr = users.map {|i| i.id}
-    errors.add(:base, "already in group") if arr.select{|user| arr.count(user) > 1 }.count > 1
-  end
+  # before_save :user_in_group
+
+  # def user_in_group
+  #   arr = users.map {|i| i.id}
+  #   errors.add(:base, "already in group") if (arr.select{|u| arr.count(u) > 1 }.count > 1)
+  # end
 
   # before_save do
   #   raise "no users in group!" if users.empty?
--- app/models/grouping.rb
+++ app/models/grouping.rb
@@ -3,10 +3,18 @@ class Grouping < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
 
-  before_save :already_in_group?
+  # validates :user_id, uniqueness: { scope: :group,
+  #    message: "User already in group" }
 
-  def already_in_group?
-    errors.add(:base, "User already in group") if group.users.include?(user)
-  end
+  validates_uniqueness_of :user_id, :scope => :group_id
+
+  # before_save :already_in_group?
+
+  # def already_in_group?
+  #   errors.add(:base, "User already in group") if self.include?(user_id) && self.include?(group_id) 
+
+
+  #   # group.users.include?(user) && 
+  # end
 
 end
