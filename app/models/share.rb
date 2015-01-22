4e721d9fe6321d01d5a0983b234763da3fb29915
Andres Vara <andres.vara@gmail.com>
2015-01-22 15:22:35 +0000

    added validation to share 0 - 100

--- app/models/share.rb
+++ app/models/share.rb
@@ -3,13 +3,7 @@ class Share < ActiveRecord::Base
   belongs_to :bill_type
   belongs_to :grouping
 
-  # validates :bill_type, :uniqueness => {:scope =>  :grouping }
   validates :bill_type, uniqueness: { scope: :grouping }
-
-  # validate :bill_type_and_grouping_unique?
-
-  # def bill_type_and_grouping_unique?
-  #   errors.add(:base, "cannot be twice")  if Share.where(bill_type_id: bill_type_id, grouping_id: grouping_id).present?
-  # end
+  validates :percent, numericality: { greater_than: 0, less_than_or_equal_to: 100}
 
 end
